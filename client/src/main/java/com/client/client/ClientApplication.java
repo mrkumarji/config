7118f89b336b7f6f0d9fa8e0877531a7f4fb4415
mrkumarji <mrkumarji@yahoo.com>
2022-04-29 00:03:17 -0500

    s

---
+++ client/src/main/java/com/client/client/ClientApplication.java
@@ -0,0 +1,27 @@
+package com.client.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RefreshScope
+@RestController
+public class ClientApplication {
+
+	@Value("${message: Default Hello}")
+	private String message;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String message() {
+		return message;
+	}
+}
